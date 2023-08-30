af53a61c088105f9a8fa9265925a2f9d9f982e4f
josafat <josa221001@gmail.com>
2023-08-29 18:11:00 -0600

    fix: reparando conflictos del prefijo

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
   const configService = app.get(ConfigService);
+
   await app.listen(3000);
 
   console.log(`Application running on: ${await app.getUrl()}`)
